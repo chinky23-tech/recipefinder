dcc66d83cd0740e0dd8c017ba1252d1ef8957ba6
chinky23-tech <chinky0330@gmail.com>
2025-11-28 19:18:04 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -109,7 +109,8 @@ const Navbar = () => {
 
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
-     
+     <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
       
         </ul>
       )}
