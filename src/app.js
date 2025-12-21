8c5716b46fb9a6baf144e0433b0fda78b1149e79
chinky23-tech <chinky0330@gmail.com>
2025-12-21 23:08:47 +0530

    update

--- src/app.js
+++ src/app.js
@@ -4,12 +4,3 @@ function hello(){
 }
 setTimeout(hello , 2000);
 
-function world(){
-    console.log("world");
-}
-setTimeout(world, 3000);
-
-function math(){
-    console.log("math");
-}
-setTimeout(math , 1000);
