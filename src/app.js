c6f9136e0972d97695050196e24f2126757eeea9
chinky23-tech <chinky0330@gmail.com>
2025-12-25 23:09:24 +0530

    update

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ function hello(){
     console.log("Hello, World!");
     console.log("This message is displayed after a 2-second delay.");
     console.log("Enjoy coding!");
-    console.log("Hello, World!");
-    console.log("This message is displayed after a 2-second delay.");
-    console.log("Enjoy coding!");
+    
     
 }
 setTimeout(hello , 2000);
