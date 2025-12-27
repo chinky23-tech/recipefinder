327797b24bd7abfd79a186372487414610189404
chinky23-tech <chinky0330@gmail.com>
2025-12-27 23:51:18 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
     return (
         <footer className="bg-gray-900 text-white py-4 text-center">
             © 2025 Your Website. All rights reserved.
+             © 2025 Your Website. All rights reserved.
         </footer>
     );
 }
