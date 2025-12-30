76c99fb14a32270374df7bbe695c34c42b49b33e
chinky23-tech <chinky0330@gmail.com>
2025-12-30 13:40:47 +0530

    update

--- src/Pages/Lunch.jsx
+++ src/Pages/Lunch.jsx
@@ -2,7 +2,7 @@ export default function Lunch() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Lunch Recipes</h1>
-    
+    <h2 className="text-xl mt-4">Discover delicious lunch ideas to fuel your day!</h2>
         
     </div>
   );
