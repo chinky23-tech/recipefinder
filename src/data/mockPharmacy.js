5766610bb1dbaf4c0ccd91a0317afe7b69db6562
chinky23-tech <chinky0330@gmail.com>
2025-12-31 20:43:31 +0530

    update

--- src/data/mockPharmacy.js
+++ src/data/mockPharmacy.js
@@ -2,6 +2,5 @@ export const mockPharmacies = [
 { id: 1, name: " Pharmacy 1" },
 { id: 2, name: "Pharmacy 2" },
 { id: 3, name: "Pharmacy 3" },
-{ id: 3, name: "Pharmacy 4" },
-{ id: 3, name: "Pharmacy 4" },
+
 ];
