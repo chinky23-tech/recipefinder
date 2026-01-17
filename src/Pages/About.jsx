57dc8edce00bbe0796d1474444f237a201b858a2
chinky23-tech <chinky0330@gmail.com>
2026-01-17 23:13:26 +0530

    update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,8 @@
-export default function Lunch() {
+export default function About() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Lunch Recipes</h1>
-      <h1>welcome to recipefinder</h1>
+      <h1 className="text-3xl font-bold">About Recipe Finder</h1>
+      <p>Welcome to Recipe Finder, your go-to app for discovering delicious recipes!</p>
     </div>
   );
 }
