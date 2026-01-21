80199806b10056655c67778204812f1aff801230
chinky23-tech <chinky0330@gmail.com>
2026-01-21 23:17:31 +0530

    update

--- src/Pages/Dinner.jsx
+++ src/Pages/Dinner.jsx
@@ -1,7 +1,7 @@
-export default function Lunch() {
+export default function Dinner() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Lunch Recipes</h1>
+      <h1 className="text-3xl font-bold">Dinner Recipes</h1>
     </div>
   );
 }
