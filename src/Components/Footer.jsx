f67a68c397e9fd2565c31d0f3764f42536a7a721
chinky23-tech <chinky0330@gmail.com>
2026-02-09 21:42:49 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="bg-gradient-to-r from-orange-400 to-red-500 text-white py-8 text-center shadow-lg mt-12">
+        <footer className="bg-linear-to-r from-orange-400 to-red-500 text-white py-8 text-center shadow-lg mt-12">
             <div className="max-w-7xl mx-auto px-4">
                 <p>© 2025 RecipeHub. All rights reserved.</p>
                 <p className="text-sm text-orange-100 mt-2">Built with Love</p>
